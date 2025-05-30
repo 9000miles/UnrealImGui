a4b3d220a7e9e0d8bb1edef8f6646bbc30c1367f
JJCoder <JJCoder@openzilab.com>
2025-05-30 21:39:59 +0800

    refactor(UnrealImGui): 修改颜色转换函数参数命名
    - 将 ColorConvertU32ToFloat4 和 ColorConvertFloat4ToU32 函数的参数名从 in 改为 In
    - 这个改动统一了参数命名风格，使其与其他 API 函数保持一致

--- Source/ThirdParty/ImGuiLibrary/Include/imgui.h
+++ Source/ThirdParty/ImGuiLibrary/Include/imgui.h
@@ -941,8 +941,8 @@ namespace ImGui
     IMGUI_API ImVec2        CalcTextSize(const char* text, const char* text_end = NULL, bool hide_text_after_double_hash = false, float wrap_width = -1.0f);
 
     // Color Utilities
-    IMGUI_API ImVec4        ColorConvertU32ToFloat4(ImU32 in);
-    IMGUI_API ImU32         ColorConvertFloat4ToU32(const ImVec4& in);
+    IMGUI_API ImVec4        ColorConvertU32ToFloat4(ImU32 In);
+    IMGUI_API ImU32         ColorConvertFloat4ToU32(const ImVec4& In);
     IMGUI_API void          ColorConvertRGBtoHSV(float r, float g, float b, float& out_h, float& out_s, float& out_v);
     IMGUI_API void          ColorConvertHSVtoRGB(float h, float s, float v, float& out_r, float& out_g, float& out_b);
 
