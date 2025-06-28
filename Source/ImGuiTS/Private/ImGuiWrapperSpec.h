f5d1525b2fbc9b8fe385724ee7e4092528595d65
lizijun <1655753014@qq.com>
2025-06-28 22:45:02 +0800

    refactor(ImGui): 优化绘图函数并注释掉无用代码
    - 在 UIStockChart.ts 中注释掉 PlotHistogram 函数调用
    - 在 ImGuiWrapperSpec.h 中修改 PlotLines 函数参数类型

--- Source/ImGuiTS/Private/ImGuiWrapperSpec.h
+++ Source/ImGuiTS/Private/ImGuiWrapperSpec.h
@@ -117,7 +117,7 @@ namespace Wrapper {
 			&cb_user_data);
 	}
 
-	static void PlotLines(const char* label, float values[], int values_count, int values_offset = 0, const char* overlay_text = NULL, float scale_min = FLT_MAX, float scale_max = FLT_MAX, ImVec2 graph_size = ImVec2(0, 0), int stride = sizeof(float))
+	static void PlotLines(const char* label, const float* values, int values_count, int values_offset = 0, const char* overlay_text = NULL, float scale_min = FLT_MAX, float scale_max = FLT_MAX, ImVec2 graph_size = ImVec2(0, 0), int stride = sizeof(float))
 	{
 		ImGui::PlotLines(label, values, values_count, values_offset, overlay_text, scale_min, scale_max, graph_size, stride);
 	}
